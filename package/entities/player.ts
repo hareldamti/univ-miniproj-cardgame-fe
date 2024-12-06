bfe1f92805aa599b0af4cd12f2bad5e853c0bf76
t-hareldamti <t-hareldamti@microsoft.com>
2024-12-06 13:07:34 +0200

    skeleton components

---
+++ package/entities/player.ts
@@ -0,0 +1,22 @@
+export type GameState = {
+    
+}
+
+export type PlayerState = {
+    Cards: GameCard[],
+    Settlements: BoardNode[],
+    Cities: BoardNode[],
+    Roads: BoardEdge[],
+}
+
+export type GameCard = {
+
+}
+
+export type BoardNode = {
+
+};
+
+export type BoardEdge = {
+
+};
